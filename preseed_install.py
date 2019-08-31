1481854786b484d76bac40e508ba4d1ac8e81108
Paweł Słowik <pawel.slowik@zoho.com>
2019-08-31 12:16:58 +0200

    Add functions for extracting kernel and initrd files from a QEMU VM image
    This is needed for booting ARM VMs, since I couldn't find any way to
    install a working bootloader from the Debian installer.

--- preseed_install.py
+++ preseed_install.py
@@ -6,7 +6,16 @@ import os.path
 import subprocess
 import re
 import tempfile
-from typing import Tuple, IO, Optional
+import shlex
+from typing import Tuple, NamedTuple, Iterable, IO, Optional
+
+Partition = NamedTuple("Partition", [
+    ("type", str),
+    ("sector_size", int),
+    ("start_sector", int),
+    ("size_in_sectors", int),
+    ("bootable", bool),
+])
 
 def install(
         iso_filename: str,
@@ -95,6 +104,174 @@ def create_image(filename: str, size: str) -> None:
         raise ValueError("already exists: %s" % filename)
     command = ["qemu-img", "create", "-f", "qcow2", filename, size]
     subprocess.run(command, check=True)
+
+def extract_boot_files(image_filename: str) -> None:
+    """Extract the Linux kernel and initrd files from a QEMU VM image.
+
+    Will not overwrite an existing file."""
+    if not os.path.exists(image_filename):
+        raise ValueError("file not found: %s" % image_filename)
+    image_base, _ = os.path.splitext(image_filename)
+    kernel_filename = image_base + ".kernel"
+    initrd_filename = image_base + ".initrd"
+    # put temporary files on the same filesystem, because they might be too
+    # large to fit elsewhere
+    tmp_dir_base = os.path.dirname(os.path.realpath(image_filename))
+    with tempfile.TemporaryDirectory(dir=tmp_dir_base) as tmp_dir:
+        raw_filename = os.path.join(tmp_dir, image_base + ".raw")
+        partition_filename = os.path.join(tmp_dir, image_base + ".bootable")
+        image_to_raw(image_filename, raw_filename)
+        extract_boot_partition(raw_filename, partition_filename)
+        extract_partition_boot_files(partition_filename, kernel_filename, initrd_filename)
+
+def image_to_raw(input_filename: str, output_filename: str) -> None:
+    """Convert a QEMU disk image to raw format.
+
+    Will not overwrite an existing file."""
+    if not os.path.exists(input_filename):
+        raise ValueError("file not found: %s" % input_filename)
+    if os.path.exists(output_filename):
+        raise ValueError("already exists: %s" % output_filename)
+    command = ["qemu-img", "convert", "-O", "raw", input_filename, output_filename]
+    subprocess.run(command, check=True)
+
+def extract_boot_partition(image_filename: str, output_filename: str) -> None:
+    """Extract the first bootable Linux partition from a disk image file.
+
+    Will not overwrite an existing file. Does not support LVM."""
+    if not os.path.exists(image_filename):
+        raise ValueError("file not found: %s" % image_filename)
+    if os.path.exists(output_filename):
+        raise ValueError("already exists: %s" % output_filename)
+    for partition in list_partitions(image_filename):
+        if partition.type == "Linux" and partition.bootable:
+            # use dd for sparse file support
+            command = [
+                "dd",
+                "if=%s" % image_filename,
+                "of=%s" % output_filename,
+                "bs=%d" % partition.sector_size,
+                "skip=%d" % partition.start_sector,
+                "count=%d" % partition.size_in_sectors,
+                "conv=sparse",
+            ]
+            subprocess.run(command, check=True, stderr=subprocess.DEVNULL)
+            return
+    raise ValueError("no bootable Linux partition found")
+
+def list_partitions(image_filename: str) -> Iterable[Partition]:
+    """List partitions in a disk image.
+
+    Does not support LVM."""
+    if not os.path.exists(image_filename):
+        raise ValueError("file not found: %s" % image_filename)
+    parser = [
+        ("Type", r".+", lambda s: s.strip()),
+        ("Start", r"[0-9]+", int),
+        ("Sectors", r"[0-9]+", int),
+        ("Boot", r"\*{0,1}", bool),
+    ]
+    column_spec = ",".join(header for header, _, _ in parser)
+    header_regexp = r"\s+".join(header for header, _, _ in parser)
+    data_regexp = r"\s+".join("(?P<%s>%s)" % (header, regexp) for header, regexp, _ in parser)
+    postprocessors = {header: func for header, _, func in parser}
+    command = ["/sbin/fdisk", "-l", "-o", column_spec, image_filename]
+    process = subprocess.run(command, capture_output=True, text=True, check=True)
+    sector_size = parse_fdisk_sector_size(process.stdout)
+    units = parse_fdisk_units(process.stdout)
+    if sector_size != units:
+        raise ValueError(
+            "partition table sector size and unit size differ: %d != %d"
+            % (sector_size, units)
+        )
+    in_list = False
+    for line in process.stdout.splitlines():
+        if re.fullmatch(header_regexp, line):
+            in_list = True
+            continue
+        if in_list:
+            match = re.fullmatch(data_regexp, line)
+            if not match:
+                raise ValueError("unable to parse partition line: %s" % line)
+            parsed = {
+                header: postprocessors[header](value) # type: ignore
+                for header, value in match.groupdict().items()
+            }
+            yield Partition(
+                sector_size=sector_size,
+                type=parsed["Type"],
+                start_sector=parsed["Start"],
+                size_in_sectors=parsed["Sectors"],
+                bootable=parsed["Boot"],
+            )
+
+def parse_fdisk_units(fdisk_output: str) -> int:
+    """Parse unit size in bytes from fdisk output."""
+    regexp = r"^Units: sectors of [0-9]+ \* [0-9]+ = ([0-9]+) bytes$"
+    match = re.search(regexp, fdisk_output, re.MULTILINE)
+    if not match:
+        raise ValueError("can't parse partition table units")
+    return int(match.group(1))
+
+def parse_fdisk_sector_size(fdisk_output: str) -> int:
+    """Parse sector size in bytes from fdisk output."""
+    regexp = r"^Sector size \(logical/physical\): ([0-9]+) bytes / ([0-9]+) bytes"
+    match = re.search(regexp, fdisk_output, re.MULTILINE)
+    if not match:
+        raise ValueError("can't parse partition table sector size")
+    if match.group(1) != match.group(2):
+        raise ValueError(
+            "partition table logical and physical sector sizes differ: %s != %s"
+            % (match.group(1), match.group(2))
+        )
+    return int(match.group(1))
+
+def extract_partition_boot_files(
+        partition_filename: str,
+        output_kernel_filename: str,
+        output_initrd_filename: str,
+    ) -> None:
+    """Extract the Linux kernel and initrd files from a partition image.
+
+    Will not overwrite an existing file."""
+    if not os.path.exists(partition_filename):
+        raise ValueError("file not found: %s" % partition_filename)
+    if os.path.exists(output_kernel_filename):
+        raise ValueError("already exists: %s" % output_kernel_filename)
+    if os.path.exists(output_initrd_filename):
+        raise ValueError("already exists: %s" % output_initrd_filename)
+    kernel = parse_symlink_target(debugfs_command(partition_filename, "stat /vmlinuz"))
+    initrd = parse_symlink_target(debugfs_command(partition_filename, "stat /initrd.img"))
+    debugfs_command(partition_filename, "dump %s %s" % (kernel, output_kernel_filename))
+    debugfs_command(partition_filename, "dump %s %s" % (initrd, output_initrd_filename))
+
+def debugfs_command(partition_filename: str, command: str) -> str:
+    """Run a debugfs command."""
+    if not os.path.exists(partition_filename):
+        raise ValueError("file not found: %s" % partition_filename)
+    cmd = ["/sbin/debugfs", "-f", "-", partition_filename]
+    process = subprocess.Popen(
+        cmd,
+        universal_newlines=True,
+        stdin=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    )
+    stdout, stderr = process.communicate(command)
+    if process.returncode != 0:
+        raise subprocess.CalledProcessError(process.returncode, cmd, stdout, stderr)
+    return stdout
+
+def parse_symlink_target(debugfs_output: str) -> str:
+    """Extract symlink target from debugfs stat command output."""
+    regexp = "^Fast link dest: (\".+\")$"
+    match = re.search(regexp, debugfs_output, re.MULTILINE)
+    if not match:
+        raise ValueError("can't parse symlink target")
+    split_to_unquote = shlex.split(match.group(1))
+    if len(split_to_unquote) != 1:
+        raise ValueError("can't parse symlink target")
+    return split_to_unquote[0]
 
 def named_tmp(content: bytes) -> IO:
     """Create a named temporary file with given content."""
